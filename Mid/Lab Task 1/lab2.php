f0321b5baa3ed0b4ff5915da25477659357841db
Mustafa Tausif <mustafatausif2002@gmail.com>
2025-11-16 13:39:39 +0600

    first commit

--- Mid/Lab Task 1/lab2.php
+++ Mid/Lab Task 1/lab2.php
@@ -1,11 +1,52 @@
-<!DOCTYPE html>
-<html lang="en">
+git add .<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Participant Registration</title>
+    <style>
+        body{
+            font-family: Arial, sans-serif;
+            padding: 30px;
+            background-color: #f0f8ff;
+        }
+ 
+    h2 {
+      text-align: center;
+    }
+
+    form {
+      background-color: #ffffff;
+      padding: 20px;
+      border-radius: 10px;
+      width: 300px;
+      margin: 0 auto;
+      box-shadow: 0 0 10px rgba(0,0,0,0.1);
+    }
+
+    </style>
 </head>
 <body>
-    
+
+
+    <form>
+
+    <h2>Participant Registration</h1>
+
+    Full Name:<br>
+    <input type="text"> <br>
+
+    Email: <br>
+    <input type="text"> <br>
+
+    Phone Number: <br>
+    <input type="number"> <br>
+
+    Password: <br>
+    <input type="password"> <br>
+
+    Confirm Password: <br>
+    <input type="password"> <br>
+    </form>
+
 </body>
 </html>
+
