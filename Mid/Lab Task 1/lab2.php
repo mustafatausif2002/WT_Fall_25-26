d932ce35d0af9d738e171bac7073adffe8fbc5a2
Mustafa Tausif <mustafatausif2002@gmail.com>
2025-11-16 14:18:51 +0600

    lab task2

--- Mid/Lab Task 1/lab2.php
+++ Mid/Lab Task 1/lab2.php
@@ -1,4 +1,4 @@
-git add .<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <title>Participant Registration</title>
@@ -17,9 +17,36 @@ git add .<!DOCTYPE html>
       background-color: #ffffff;
       padding: 20px;
       border-radius: 10px;
-      width: 300px;
+      width: 400px;
       margin: 0 auto;
       box-shadow: 0 0 10px rgba(0,0,0,0.1);
+    }
+
+    button {
+      background-color: #1f75caff;
+      color: white;
+      cursor: pointer;
+      margin: 10px 0 0 0;
+    }
+
+    input{
+      width: 100%;
+      padding: 8px;
+      margin-top: 10px;
+      border-radius: 5px;
+      border: 1px solid #ccc;
+    }
+    #output {
+      margin-top: 20px;
+      text-align: center;
+      font-size: 16px;
+      color: #003366;
+    }
+ 
+    #error {
+      margin-top: 10px;
+      color: red;
+      text-align: center;
     }
 
     </style>
@@ -27,26 +54,79 @@ git add .<!DOCTYPE html>
 <body>
 
 
-    <form>
+    <form onsubmit="return handleSubmit()">
 
     <h2>Participant Registration</h1>
 
     Full Name:<br>
-    <input type="text"> <br>
+    <input type="text" id="name"> <br>
 
     Email: <br>
-    <input type="text"> <br>
+    <input type="text" id="email"> <br>
 
     Phone Number: <br>
-    <input type="number"> <br>
+    <input type="number" id="phone"> <br>
 
     Password: <br>
-    <input type="password"> <br>
+    <input type="password" id="password"> <br>
 
     Confirm Password: <br>
-    <input type="password"> <br>
+    <input type="password" id="confirmpassword"> <br>
+
+    <button type="submit">Register</button>
     </form>
+
+
+
+
+  <div id="error"></div>
+  <div id="output"></div>
+
+    <script>
+    function handleSubmit() {
+ 
+      var name = document.getElementById("name").value.trim();
+      var email = document.getElementById("email").value.trim();
+      var phone = document.getElementById("phone").value.trim();
+      var password = document.getElementById("password").value;
+      var confirmpassword = document.getElementById("confirmpassword").value;
+ 
+      var errorDiv = document.getElementById("error");
+      var outputDiv = document.getElementById("output");
+ 
+
+      errorDiv.innerHTML = "";
+      outputDiv.innerHTML = "";
+ 
+
+      if (name === "" || email === "" || phone === "" || password === "" || confirmpassword === "") {
+        errorDiv.innerHTML = "Please fill in all fields.";
+        return false;
+      }
+ 
+      if (email.includes("@")) {
+        errorDiv.innerHTML = " Email must be @";
+        return false;
+      }
+
+      if (isNaN(phonenumber)) {
+        errorDiv.innerHTML = " Phone Numbe must be numeric.";
+        return false;
+      }
+ 
+ 
+      outputDiv.innerHTML = `
+        <strong>Registration Successful!</strong><br><br>
+        Name: ${name}<br>
+        Email: ${email}<br>
+        Phone: ${phone}<br>
+      `;
+ 
+      return false;
+    }
+  </script>
 
 </body>
 </html>
+
 
