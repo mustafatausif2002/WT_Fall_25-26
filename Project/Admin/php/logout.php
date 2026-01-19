8938884d5dfe8a5ce4f26ddf8b2a52533ffeb3b0
Mustafa Tausif <mustafatausif2002@gmail.com>
2026-01-19 17:34:22 +0600

    modify cookie

--- Project/Admin/php/logout.php
+++ Project/Admin/php/logout.php
@@ -3,6 +3,9 @@
 session_start();
 session_destroy();
 
+setcookie("admin_email", "", time() - 3600, "/");
+setcookie("admin_name", "", time() - 3600, "/");
+
 header("Location: ../html/login.html");
 exit();
 
